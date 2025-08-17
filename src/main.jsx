34cdb48693a80df6fb63f600e2ba1f696a519b0a
krHimanshu123 <162711386+krHimanshu123@users.noreply.github.com>
2025-08-17 22:20:15 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,6 @@ import './index.css'
 import App from './App.jsx'
 import { CartProvider } from "./context/CartContext";
 
-
 createRoot(document.getElementById('root')).render(
   <CartProvider>
   <App/>
