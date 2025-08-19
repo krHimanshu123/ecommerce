76fca69614ef22204d381a9812cccfd905d09c66
krHimanshu123 <162711386+krHimanshu123@users.noreply.github.com>
2025-08-19 23:59:36 +0530

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,7 +4,6 @@ const Home = () => {
   return (
     <div class="grid-layout">
 
-      
         <div class="product">
             <img src="rose.jpg" alt="Product 1" />
             <h4>Product 1</h4>
