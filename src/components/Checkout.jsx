47e9ee67961ee18ac0d5b478e93fc3e2f08769ba
krHimanshu123 <162711386+krHimanshu123@users.noreply.github.com>
2025-08-24 00:15:30 +0530

    Update Checkout.jsx

--- src/components/Checkout.jsx
+++ src/components/Checkout.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { processPayment } from "../services/paymentService";
 import { useNavigate } from "react-router-dom";
 
+
 const Checkout = () => {
   const [cardNumber, setCardNumber] = useState("");
   const [cvv, setCvv] = useState("");
