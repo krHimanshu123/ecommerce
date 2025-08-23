a3242cc9999100fad33fe3e109e90e3d40e84bc9
krHimanshu123 <162711386+krHimanshu123@users.noreply.github.com>
2025-08-24 00:21:42 +0530

    Update cartService.js

--- src/services/cartService.js
+++ src/services/cartService.js
@@ -11,7 +11,6 @@ export const addToCart = async (userId, productId) => {
   });
 };
 
-
 export const getCartItems = async (userId) => {
   const response = await axios.get(`${CART_API_URL}/user/${userId}`);
   return response.data;
