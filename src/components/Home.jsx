b089372a1f03148659a8ded0f4b5dc14ef419375
krHimanshu123 <162711386+krHimanshu123@users.noreply.github.com>
2025-10-16 23:17:32 +0530

    Add Product 4 to Home component

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -18,6 +18,7 @@ const Home = () => {
             <h4>Product 3</h4>
             <p>$10.00</p>
             </div>
+      
           <div class="product">
             <img src="rose.jpg" alt="Product 1" />
             <h4>Product 4</h4>
